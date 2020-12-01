f4c45df983a6f87c595dad8aada614d77b9b751e
Jordan Kasper <jordan@jordankasper.com>
2020-12-01 07:33:56 -0500

    day01

--- 01.py
+++ 01.py
@@ -3,4 +3,17 @@ from helpers import read_data_file
 
 print('\n************* Day 01 *************\n')
 
-print(read_data_file(sys.argv[1], 'comma'))
+expenses = list(map(lambda n: int(n), read_data_file(sys.argv[1])))
+answer = None
+
+for i in range(len(expenses)):
+    j = i + 1
+    while j < len(expenses):
+        k = j + 1
+        while k < len(expenses):
+            if ((expenses[i] + expenses[j] + expenses[k]) == 2020):
+                answer = expenses[i] * expenses[j] * expenses[k]
+            k = k + 1
+        j = j + 1
+
+print(answer)
