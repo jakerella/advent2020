04cb8e808764b4238971f3908e6fa29351c14d05
Jordan Kasper <jordan@jordankasper.com>
2020-12-01 08:50:01 -0500

    Update helpers to handle more cases

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 
-def read_data_file(filename, method='lines'):
+def read_data_file(filename, method='lines', convert=None):
     try:
         with open(filename, encoding = 'utf-8') as f:
             data = []
@@ -12,10 +12,19 @@ def read_data_file(filename, method='lines'):
                 else:
                     raise ValueError('Invalid parse method')
 
+            if (convert):
+                if (type(data[0]) is str):
+                    data = list(map(convert, data))
+                elif(type(data[0]) is list):
+                    for i in range(len(data)):
+                        if (callable(convert)):
+                            data[i] = list(map(convert, data[i]))
+                        elif(type(convert) is list):
+                            for j in range(len(convert)):
+                                data[i][j] = convert[j](data[i][j])
+            
             return data
 
     except Exception as err:
         print('ERROR reading file', filename)
         print(err)
-
-# print(data_file('day01/input.txt', 'comma'))
